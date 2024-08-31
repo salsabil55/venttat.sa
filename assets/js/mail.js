8501b3801291d084bf50cd26bea03a5d8e0b6931
salsabil <salsabil.tarek55@gmail.com>
2024-08-31 11:43:43 +0300

    email

---
+++ assets/js/mail.js
@@ -0,0 +1,15 @@
+function sendMail() {
+  const params = {
+    from_name: document.getElementById("funame").value,
+    email_id: document.getElementById("email_id").value,
+    tel_id: document.getElementById("tel").value,
+    event_id: document.getElementById("event").value,
+    service_id: document.getElementById("service").value,
+  };
+  email
+    .send(service_yxvzbi7, template_b3vmq1a, params)
+    .then(function redirectToHome() {
+      window.location.href = "/"; // Redirects to the home page
+    });
+  redirectToHome();
+}
