3e213b78bdf1bc28429b38838aaffb97aa99635a
salsabil <salsabil.tarek55@gmail.com>
2024-08-31 12:44:50 +0300

    'edit'

--- assets/js/mail.js
+++ assets/js/mail.js
@@ -1,21 +1,19 @@
-// function sendMail() {
-//   var params = {
-//     from_name: document.getElementById("funame").value,
-//     email_id: document.getElementById("email_id").value,
-//     tel_id: document.getElementById("tel").value,
-//     event_id: document.getElementById("event").value,
-//     service_id: document.getElementById("service").value,
-//   };
+function sendMail() {
+  var params = {
+    from_name: document.getElementById("funame").value,
+    email_id: document.getElementById("email_id").value,
+    tel_id: document.getElementById("tel").value,
+    event_id: document.getElementById("event").value,
+    service_id: document.getElementById("service").value,
+  };
 
-//   emailjs
-//     .send("service_yxvzbi7", "template_b3vmq1a", params)
-//     .then((res) => {
-//       console.log("sucess sent msg", res);
-//       window.location.href = "https://salsabil55.github.io/venttat.sa/"; // Redirects to the home page
-//     })
-//     .catch((error) => {
-//       console.error("Error:", error);
-//     });
-// }
-const test = document.getElementById("event").target.value;
-console.log(test);
+  emailjs
+    .send("service_yxvzbi7", "template_b3vmq1a", params)
+    .then((res) => {
+      console.log("sucess sent msg", res);
+      window.location.href = "https://salsabil55.github.io/venttat.sa/"; // Redirects to the home page
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+}
